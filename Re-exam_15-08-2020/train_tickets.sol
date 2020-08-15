92dd13ee3b310046ea7b23b3d33aa771031bbfd8
Simi-Paxleal-J <59289710+Simi-Paxleal-J@users.noreply.github.com>
2020-08-15 20:57:38 +0530

    Update train_tickets.sol

--- Re-exam_15-08-2020/train_tickets.sol
+++ Re-exam_15-08-2020/train_tickets.sol
@@ -3,17 +3,17 @@ pragma solidity ^0.4.21;
 contract Trainticket{
     string name;
     uint tick_No;
-    string addrss;
+    string addr;
     string source_addrss;
     string dest_addrss;
     string train_platform;
     uint source_time;
     uint dest_time;
     bool status;
-    constructor(string passengerName, uint tickNumber, string trainBatch, string passengerAddress, string sAddress, string dAddress, string trainPlatform, uint stime, uint dtime, bool studStatus) public{
+    constructor (string passengerName, uint tickNumber, string trainBatch, string passengerAddress, string sAddress, string dAddress, string trainPlatform, uint stime, uint dtime, bool trainStatus) public{
         name = passengerName;
-        tick_No = studRollNumber;
-        addrss = passenger_address;
+        tick_No = tickNumber;
+        addr = passengerAddress;
         source_addrss = sAddress;
         dest_addrss = dAddress;
         train_platform = trainPlatform;
@@ -22,8 +22,7 @@ contract Trainticket{
         status = trainStatus;
     }
     
-    function getLiveTrainTicket() public view returns(string, uint, string, string, string, string, uint, uint, uint, bool){
-        return(name, tick_No, addrss, aource_address, dest_addrss, train_platform, source_time, dest_time, status);
+    function getLiveTrainTicket() public view returns(string, uint, string, string, string, string, uint, uint, uint, bool) {
+        return(name, tick_No, addr, aource_addrss, dest_addrss, train_platform, source_time, dest_time, status);
     }
 }
-
