40fc62f4892c5257dee8c24db8571a203cb770f6
Simi-Paxleal-J <59289710+Simi-Paxleal-J@users.noreply.github.com>
2020-08-15 16:15:07 +0530

    Create train_tickets.sol
    A smart contract for creating and getting report card of a student.
    
    Test Deployment of the smart contract can be found at: Contract id: 0xF0960cA9803258aAd3f246396C19f46C8Df5dbFA https://ropsten.etherscan.io/tx/0x4ee825b84398ea4033bd6273a3ee6e2821e301bc825a9b1d514663d927c2aa38

---
+++ Re-exam_15-08-2020/train_tickets.sol
@@ -0,0 +1,29 @@
+pragma solidity ^0.4.21;
+
+contract Trainticket{
+    string name;
+    uint tick_No;
+    string address;
+    string source_addrss;
+    string dest_addrss;
+    string train_platform;
+    uint source_time;
+    uint dest_time;
+    bool status;
+    constructor(string passengerName, uint tickNumber, string trainBatch, string passengerAddress, string sAddress, string dAddress, string trainPlatform, uint stime, uint dtime, bool studStatus) public{
+        name = passengerName;
+        tick_No = studRollNumber;
+        address = passenger_address;
+        source_addrss = sAddress;
+        dest_addrss = dAddress;
+        train_platform = trainPlatform;
+        source_time = stime;
+        dest_time = dtime;
+        status = trainStatus;
+    }
+    
+    function getLiveTrainTicket() public view returns(string, uint, string, string, string, string, uint, uint, uint, bool){
+        return(name, tick_No, address, aource_address, dest_addrss, train_platform, source_time, dest_time, status);
+    }
+}
+© 2020 GitHub, Inc.
